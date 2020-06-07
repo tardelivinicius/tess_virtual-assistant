614f38b24d357e354bb59be4997e8ff1e86bd9cb
Vinícius Valle <tardelivinicius@gmail.com>
2020-06-07 14:26:05 -0300

    Add new commands

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def gtts_playsound(audio_name, text):
     # Reproduz uma Mensagem de boas vindas
     playsound('audios/{0}.mp3'.format(audio_name))
 
-def send_command(micro):
+def send_command(micro, result):
     waiting_command = True
     while(waiting_command is True):
         with sr.Microphone() as source:
@@ -42,29 +42,53 @@ def send_command(micro):
     # Faça o comando
     if 'placa de vídeo' in command:
         gtts_playsound('vga_check', 'Abrindo dados da sua placa de vídeo')
-        s.Popen('C:\Program Files\AMD\CNext\CNext\RadeonSoftware.exe')
+        s.Popen('C:/Program Files/AMD/CNext/CNext/RadeonSoftware.exe')
     elif 'desempenho' in command:
         gtts_playsound('check_system', 'Abrindo dados de Hardware')
         s.Popen('C:/Program Files/WindowsApps/Microsoft.XboxGamingOverlay_5.220.4292.0_x64__8wekyb3d8bbwe/GameBar.exe')
     elif 'Call of Duty' in command or 'call of duty' in command:
         gtts_playsound('cod_warzone', 'Abrindo Call of Duty Warzone')
-        s.Popen('"C:\Program Files (x86)\Call of Duty Modern Warfare\Modern Warfare Launcher.exe"')
+        s.Popen('"C:/Program Files (x86)/Call of Duty Modern Warfare/Modern Warfare Launcher.exe"')
     elif 'GTA' in command or 'gta' in command:
         gtts_playsound('gta', 'Abrindo GTA 5')
         s.Popen('com.epicgames.launcher://apps/9d2d0eb64d5c44529cece33fe2a46482?action=launch&silent=true')
-    elif 'previsão' in command:
+    elif 'previsão' in command or 'Bom dia' in command:
         response = requests.get('https://api.hgbrasil.com/weather', allow_redirects=False)
         forecasts = json.loads(response.content)['results']['forecast']
         # Pega a data atual e converte em (dia/mes ex 06/06)
         date_now = datetime.now().strftime('%d/%m')
         for forecast in forecasts:
             if str(forecast['date']) == str(date_now):
-                gtts_playsound('temperature', 'A previsão do tempo para o dia de hoje é máxima de {0} graus e mínima de {1} graus com chances de {2}'.format(forecast['max'], forecast['min'], forecast['description']))
+                if 'Bom dia' in command:
+                    gtts_playsound('temperature', '''
+                    Bom dia senhor {0}. Hoje é dia {1} A previsão do tempo para o dia de hoje é máxima de {2} graus e mínima de {3} graus com chances de {4}'''.format(
+                        result[1],
+                        date_now,
+                        forecast['max'],
+                        forecast['min'],
+                        forecast['description']))
+                else:
+                    gtts_playsound('temperature', 'A previsão do tempo para o dia de hoje é máxima de {0} graus e mínima de {1} graus com chances de {2}'.format(forecast['max'], forecast['min'], forecast['description']))
                 break
-        send_command(micro)
+    elif 'ambiente' in command:
+        gtts_playsound('workdesk', 'Preparando o seu ambiente de trabalho, tenha um ótimo trabalho!')
+        # Abrir Visual Studio
+        s.Popen(['C:/Users/Vinícius Valle/AppData/Local/Programs/Microsoft VS Code/Code.exe',
+                #  '-g C:/Users/Vinícius Valle/Documents/Take5/take5_api',
+                #  '-g C:/Users/Vinícius Valle/Documents/Take5/etraining-vue'
+                ])
+        # Abrir GitHub
+        s.Popen('C:/Users/Vinícius Valle/AppData/Local/GitHubDesktop/GitHubDesktop.exe')
+        # Abrir HeidiSQL
+        s.Popen('C:/Program Files/HeidiSQL/heidisql.exe')
+        # Abrir Chrome
+        s.Popen(['C:/Program Files (x86)/Google/Chrome/Application/chrome.exe', 'https://meet.google.com/pyf-vxry-nnc?authuser=1', 'gmail.com', 'take5.monday.com'])
+        # Abrir Telegram
+        s.Popen('C:/Users/Vinícius Valle/AppData/Roaming/Telegram Desktop/Telegram.exe')
     elif 'desligar' in command:
         remove_all_audios()
         exit()
+    send_command(micro, result)
 
 def authenticated():
     gtts_playsound('ready_to_voice', 'Informe o seu código de acesso')
@@ -83,7 +107,7 @@ def initial_system():
     ----------------------------------------------------------------------------
     ''')
     # Mensagem de boas vindas do sistema
-    gtts_playsound('hello', 'Bem vindo ao Tess')
+    gtts_playsound('hello', 'Bem vindo à Tess')
 
     # Instancia microfone
     micro = sr.Recognizer()
@@ -96,7 +120,7 @@ def initial_system():
     if result:
         print('Acesso liberado...')
         gtts_playsound('authenticated', 'Código de acesso aceito, seja bem vindo {0}'.format(result[1]))
-        send_command(micro)
+        send_command(micro, result)
     else:
         gtts_playsound('not_authenticated', 'Código de acesso inválido, tente novamente')
         authenticated(micro)
